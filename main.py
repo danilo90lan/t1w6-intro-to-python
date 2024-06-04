70a6cc0679e4263556b6a92a9ca45d2e2a5599ce
danilo90lan <15630@coderacademy.edu.au>
2024-06-04 21:05:03 +1000

    logical operators

--- main.py
+++ main.py
@@ -1,2 +1,7 @@
-print("Hello world")
-print(3-5)
+#print("Hello world")
+#print(3-5)
+
+#Logical operatos and, or, not
+print(True and False) # AND returns True when both operators are True
+print(False or True) # OR returns True, when one or both of the operators are  True
+print(not False)
